c3d297bad2a8f682e8e69e5b89ae75de03dc37ea
Cleide Reis <cleide-ferreira21@hotmai.com>
2017-11-13 14:41:12 -0200

    Exercicio 8

---
+++ WebServiceConsumes/WebServiceConsumes/Default.aspx.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebServiceConsumes
+{
+    public partial class _Default : Page
+    {
+        HttpClient client;
+        Uri usuarioUri;
+        public _Default()
+        {
+            if (client == null)
+            {
+                client = new HttpClient();
+                client.BaseAddress = new Uri("http://turmadobem.azurewebsites.net/api/instituicao");
+                client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+            }
+
+        }
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+
+            if (!Page.IsPostBack)
+            {
+                getAll();
+            }
+
+        }
+        private void getAll()
+        {
+            //chamando a api pela url
+            System.Net.Http.HttpResponseMessage response = client.GetAsync("http://turmadobem.azurewebsites.net/api/instituicao").Result;
+
+            //se retornar com sucesso busca os dados
+            if (response.IsSuccessStatusCode)
+            {
+                //pegando o cabeçalho
+                usuarioUri = response.Headers.Location;
+
+                //Pegando os dados do Rest e armazenando na variável usuários
+                var usuarios = response.Content.ReadAsAsync<IEnumerable<Instituicao>>().Result;
+
+                //preenchendo a lista com os dados retornados da variável
+                GridView1.DataSource = usuarios;
+                GridView1.DataBind();
+            }
+
+            //Se der erro na chamada, mostra o status do código de erro.
+            else
+                Response.Write(response.StatusCode.ToString() + " - " + response.ReasonPhrase);
+        }
+    }
+}
---
+++ WebServiceConsumes/WebServiceConsumes/Instituicao.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebServiceConsumes
+{
+    public class Instituicao
+    {
+        [Key]
+        public String cepInstituicao { get; set; }
+        public String cidadeInstituicao { get; set; }
+        public String enderecoInstituicao { get; set; }
+        public String estadoInstituicao { get; set; }
+        public String idInstituicao { get; set; }
+        public String idStatusInstituicao { get; set; }
+        public String nomeInstituicao { get; set; }
+        public String telefoneInstituicao { get; set; }
+    }
+}
